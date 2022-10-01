66b97dd5f7856efefd8f3ef9afba7bd191b42e65
saralsy <sara.siye.liu@gmail.com>
2022-09-30 23:44:09 -0500

    Initial Commit

---
+++ DailyMeditations/DailyMeditationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyMeditationsApp.swift
+//  DailyMeditations
+//
+//  Created by Sara Liu on 9/30/22.
+//
+
+import SwiftUI
+
+@main
+struct DailyMeditationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
